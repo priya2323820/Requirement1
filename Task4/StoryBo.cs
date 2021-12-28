00417ce389c76c2e5fca7ceff73ad16e0593e2ca
priya2323820 <96436780+priya2323820@users.noreply.github.com>
2021-12-27 23:14:02 -0800

    Add files via upload

---
+++ Task4/StoryBo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requirement4
+{
+    class StoryBo
+    {
+        Story[] stories = new Story[Program.noOfStory];
+
+        public Story[] findStoryByAuthor(Story[] listOfStory, string authorNameOfStory)
+        {
+            for (int i = 0; i < listOfStory.Length; i++)
+            {
+                if (listOfStory[i].AuthorName.Equals(authorNameOfStory))
+                {
+                    stories[i] = listOfStory[i];
+                }
+            }
+
+
+            return stories;
+        }
+
+        public Story[] finfindStoryByLikes(Story[] listOfStory, int likesOfStory)
+        {
+            for (int i = 0; i < listOfStory.Length; i++)
+            {
+                if (listOfStory[i].NoOfLikes > likesOfStory)
+                {
+                    stories[i] = listOfStory[i];
+                }
+            }
+            return stories;
+        }
+    }
+}
+
