f8dca15041ba569c2d28f7cafb253577ce9743f8
priya2323820 <96436780+priya2323820@users.noreply.github.com>
2021-12-27 23:15:44 -0800

    Add files via upload

---
+++ Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+public class PasswordValidation
+{
+	public static void Main()
+	{
+		bool isValid = validatePassword();
+		if (isValid)
+		{
+			Console.WriteLine("Password is valid");
+		}
+		else
+		{
+			Console.WriteLine("Password is invalid");
+		}
+	}
+
+	public static bool validatePassword()
+	{
+		Console.WriteLine("Enter the password to be validated");
+		const int minLength = 7;
+		string password = Console.ReadLine();
+		if (password.Length > minLength)
+		{
+			if (password.Any(Char.IsWhiteSpace))
+			{
+				return false;
+			}
+			if (password.Any(Char.IsUpper) && password.Any(Char.IsLower) && password.Any(Char.IsNumber) && password.Any(ch => !Char.IsLetterOrDigit(ch)))
+			{
+				return true;
+			}
+			Console.WriteLine("statement failed");
+			return false;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
